a52e2a7eea53cadab71f6b334edf846acfc7e97b
Kingmac-coder <surjeetss1974@gmail.com>
2024-07-28 21:45:10 +0530

    Question 1 Commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          price={expense.price}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
