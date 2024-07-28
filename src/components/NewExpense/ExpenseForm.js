318ea74e2bb208e69885f798ba988548e46605bb
priyanshu1664 <priyanshusingh.ps2001@gmail.com>
2024-07-28 23:29:14 +0530

    Question 4 5 :  Commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,6 +17,19 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
